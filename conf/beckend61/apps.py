be2cbb52645b528f5fc23a16f53a35cff1c24319
brahim <brahimloca125@gmail.com>
2024-04-19 12:02:01 -0400

    fixt structur file of project

--- conf/beckend61/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Beckend61Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'beckend61'
--- done.sh
+++ done.sh
@@ -2,3 +2,17 @@
 
 docker compose run django django-admin startproject tranc .  
 docker compose run django python manage.py startapp beckend61
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 'postgres',
+#         'HOST' : 'database',
+#         'PORT' : '5432',
+#     }
+# }

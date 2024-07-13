871ffb10f3b30793ae605331ef74ed0f74af0cc0
ilkay-brahim <brahimloca125@gmail.com>
2024-07-13 12:45:12 +0100

    set up multy databas in engen

--- done.sh
+++ done.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker compose run django django-admin startproject "name_of_project" .  
-docker compose run django python manage.py startapp beckend61
-docker compose run django python manage.py createsuperuser
-docker compose run django python manage.py migrate   
-docker compose run django python manage.py makemigrations
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' container_id
+docker compose run "name of service" django-admin startproject "name_of_project" .  # create project
+docker compose run "name of service" python manage.py startapp "name of app" # create app
+docker compose run django python manage.py createsuperuser  # create super user
+docker compose run "name of service" python manage.py migrate   # apply migrations
+docker compose run "name of service" python manage.py makemigrations # create migrations
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' container_id # get ip address of container
+docker exec -it "name of contaner" psql -U "user of database" -l # list all databases
 
 
 
